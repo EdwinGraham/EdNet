bfdcda80eec9dfefe82e8ef78d4083ff14d68ab5
EdwinGraham <edwingraham1984@gmail.com>
2018-01-22 14:28:37 +0000

    v1

---
+++ R/dfBinary-data.R
@@ -0,0 +1,11 @@
+#' Random data for binary classification example.
+#'
+#' @docType data
+#'
+#' @usage data("dfBinary")
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data("dfBinary")
+"dfBinary"

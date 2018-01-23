e1b025c01007296e01970fb58e28acb9031d5b2e
EdwinGraham <edwingraham1984@gmail.com>
2018-01-23 14:34:56 +0000

    Data

--- R/dfBinary-data.R
+++ R/dfBinary-data.R
@@ -1,5 +1,15 @@
 #' Random data for binary classification example.
 #'
+#'@name dfBinary
+#'
+#'@format A data.frame with 1,000 rows and 4 variables:
+#' \itemize{
+#'   \item x1: feature 1
+#'   \item x2: feature 2
+#'   \item x3: feature 3
+#'   \item target: binary target variable
+#' }
+#'
 #' @docType data
 #'
 #' @usage data("dfBinary")
@@ -8,4 +18,4 @@
 #'
 #' @examples
 #' data("dfBinary")
-"dfBinary"
+NULL

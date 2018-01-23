c3c4816d9593d4e586f18368f0548c19fa37247a
EdwinGraham <edwingraham1984@gmail.com>
2018-01-23 16:20:40 +0000

    Gamma data

---
+++ R/dfGamma-data.R
@@ -0,0 +1,21 @@
+#' Random data for Gamma distribution regression example.
+#'
+#'@name dfGamma
+#'
+#'@format A data.frame with 1,000 rows and 4 variables:
+#' \itemize{
+#'   \item x1: feature 1
+#'   \item x2: feature 2
+#'   \item x3: feature 3
+#'   \item target: Gamma distributed target variable
+#' }
+#'
+#' @docType data
+#'
+#' @usage data("dfGamma")
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data("dfGamma")
+NULL

81700a28501091cbdcfd07e082c8d284f6ffe9ff
EdwinGraham <edwingraham1984@gmail.com>
2018-01-23 15:03:51 +0000

    Poisson data

---
+++ R/dfPoisson-data.R
@@ -0,0 +1,22 @@
+#' Random data for Poisson process regression example.
+#'
+#'@name dfPoisson
+#'
+#'@format A data.frame with 1,000 rows and 4 variables:
+#' \itemize{
+#'   \item x1: feature 1
+#'   \item x2: feature 2
+#'   \item x3: feature 3
+#'   \item exposure: length of time of Poisson process
+#'   \item target: Poisson process target variable
+#' }
+#'
+#' @docType data
+#'
+#' @usage data("dfPoisson")
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data("dfPoisson")
+NULL
